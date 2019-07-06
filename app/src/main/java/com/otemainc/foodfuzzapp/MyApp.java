3deaa3aaf6fc604b1f1f27f8e97d7e385faecaa3
TheOnlySmartBoy <o2jose43@gmail.com>
2019-07-06 11:45:56 +0300

    Completed The MyApp class code

--- app/src/main/java/com/otemainc/foodfuzzapp/MyApp.java
+++ app/src/main/java/com/otemainc/foodfuzzapp/MyApp.java
@@ -1,15 +1,17 @@
 package com.otemainc.foodfuzzapp;
 
+import android.app.Application;
 import android.content.Context;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 
-public class MyApp extends AppCompatActivity {
+public class MyApp extends Application {
     private static Context context;
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate() {
+        super.onCreate();
         context = getApplicationContext();
+
+    }
+    public static Context getContext(){
+        return context;
     }
 }
