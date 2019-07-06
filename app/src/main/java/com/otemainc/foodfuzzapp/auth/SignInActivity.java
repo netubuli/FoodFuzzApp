d30067fbc473aa5d128efb0f241b471d8ee88209
TheOnlySmartBoy <o2jose43@gmail.com>
2019-07-06 16:19:28 +0300

    Begun writing the shared preference code

--- app/src/main/java/com/otemainc/foodfuzzapp/auth/SignInActivity.java
+++ app/src/main/java/com/otemainc/foodfuzzapp/auth/SignInActivity.java
@@ -5,9 +5,12 @@ import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.otemainc.foodfuzzapp.R;
+
 public class SignInActivity extends AppCompatActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_signin);
     }
 }
---
+++ app/src/main/java/com/otemainc/foodfuzzapp/utility/SharedPreferenceUtil.java
@@ -0,0 +1,33 @@
+package com.otemainc.foodfuzzapp.utility;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.otemainc.foodfuzzapp.MyApp;
+
+public class SharedPreferenceUtil {
+    private static String PREFERENCE_NAME = "foodfuzzapp";
+    private static SharedPreferenceUtil sharedPreferenceUtil;
+    private SharedPreferences preferences;
+
+    private SharedPreferenceUtil(Context context) {
+        PREFERENCE_NAME = PREFERENCE_NAME + context.getPackageName();
+        this.preferences = context.getSharedPreferences(PREFERENCE_NAME, context.MODE_PRIVATE);
+    }
+    //get the instance of the shared preference
+    public static SharedPreferenceUtil getInstance(){
+        if(sharedPreferenceUtil == null) {
+            sharedPreferenceUtil = new SharedPreferenceUtil(MyApp.getContext());
+        }
+        return sharedPreferenceUtil;
+    }
+        //save the shared string (username)
+    public void saveString(String key, String val){
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(key,val);
+        editor.commit();
+    }
+    public String getString(String key, String defVal){
+        return preferences.getString(key,defVal);
+    }
+}
