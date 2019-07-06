19f9650ab760197ddc42219cd798fb1518cf019b
TheOnlySmartBoy <o2jose43@gmail.com>
2019-07-06 12:07:32 +0300

    Begun writting the code for splashscreen

---
+++ app/src/main/java/com/otemainc/foodfuzzapp/splash/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.otemainc.foodfuzzapp.splash;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.otemainc.foodfuzzapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_splash);
+    }
+}
