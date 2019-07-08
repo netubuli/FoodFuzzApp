b2b277d4bf8f9ac50657fa2854daabc1631eb6c4
TheOnlySmartBoy <o2jose43@gmail.com>
2019-07-08 19:49:23 +0300

    Initialized sigunp variables and set onclick listeners

--- app/src/main/java/com/otemainc/foodfuzzapp/auth/SignUpActivity.java
+++ app/src/main/java/com/otemainc/foodfuzzapp/auth/SignUpActivity.java
@@ -1,4 +1,37 @@
 package com.otemainc.foodfuzzapp.auth;
 
-public class SignUpActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.otemainc.foodfuzzapp.R;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView signUp,skip,forgetPassword;
+    EditText email, password;
+    Button signIn;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        signUp = findViewById(R.id.link_signup);
+        signIn = findViewById(R.id.btn_login);
+        skip = findViewById(R.id.skip);
+        forgetPassword = findViewById(R.id.link_forgot_pass);
+        email = findViewById(R.id.input_email);
+        password = findViewById(R.id.input_password);
+        skip.setOnClickListener(this);
+        signIn.setOnClickListener(this);
+        signUp.setOnClickListener(this);
+        forgetPassword.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        
+    }
 }
