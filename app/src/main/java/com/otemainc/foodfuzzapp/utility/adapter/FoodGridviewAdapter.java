ba0d3cb7da778b95d9519ca10370387d5b1b1d92
TheOnlySmartBoy <o2jose43@gmail.com>
2019-07-14 19:07:38 +0300

    Added Object adapters

--- app/src/main/java/com/otemainc/foodfuzzapp/utility/adapter/FoodGridviewAdapter.java
+++ app/src/main/java/com/otemainc/foodfuzzapp/utility/adapter/FoodGridviewAdapter.java
@@ -1,5 +1,38 @@
 package com.otemainc.foodfuzzapp.utility.adapter;
 
-public class FoodGridviewAdapter {
-    
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.otemainc.foodfuzzapp.utility.items.Food;
+
+import java.util.ArrayList;
+
+public class FoodGridviewAdapter extends BaseAdapter {
+    Context f;
+    ArrayList<Food> foods;
+
+    public FoodGridviewAdapter(Context f,ArrayList<Food> foods){
+        this.f = f;
+        this.foods = foods;
+    }
+    @Override
+    public int getCount() {
+        return foods.size();
+    }
+    @Override
+    public Object getItem(int position) {
+        return foods.get(position);
+    }
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup) {
+
+        return view;
+    }
 }
