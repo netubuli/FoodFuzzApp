d29ae3e03a2b5418d6f4f8c6f7aa81fa6b16ee43
TheOnlySmartBoy <o2jose43@gmail.com>
2019-11-29 13:52:54 +0300

    Added checks for user login on checkout

--- app/src/main/java/com/otemainc/foodfuzzapp/fragment/Cart.java
+++ app/src/main/java/com/otemainc/foodfuzzapp/fragment/Cart.java
@@ -29,8 +29,6 @@ public class Cart extends Fragment {
     public Cart() {
 
     }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
